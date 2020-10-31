56bfb349276fa4d6058b0c108579b10ab3732424
Richard Caceres <me@rchrd.net>
2020-10-31 00:14:41 -0700

    Add a bit

--- index.js
+++ index.js
@@ -1,32 +1,48 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 const rawFetch = async function (url) {
   const res = await fetch(url);
-  return body = await res.text();
-}
+  return (body = await res.text());
+};
 
 const jsonFetch = async function (url) {
   const res = await fetch(url);
   const body = await res.text();
   return JSON.parse(body);
-}
+};
+
+const log = console.log;
+
+const REQUIRED = "__REQUIRED__";
+
+const checkRequired = function (options) {
+  for (k in options) {
+    if (options[k] == REQUIRED) {
+      return k;
+    }
+  }
+  return null;
+};
 
 class AuthAPI {
   constructor() {
-    this.API_BASE = 'https://archive.org/account/login';
+    this.API_BASE = "https://archive.org/account/login";
   }
 }
+
+class BookReaderAPI {}
 
 class DetailsPageAPI {
   /* TODO scrape details page for display information */
   constructor() {
-    this.API_BASE = 'https://archive.org/details';
+    this.API_BASE = "https://archive.org/details";
   }
 }
 
 class FilesXmlAPI {
   constructor() {
-    this.API_PATTERN = 'https://archive.org/download/{identifier}/{identifier}_files.xml';
+    this.API_PATTERN =
+      "https://archive.org/download/{identifier}/{identifier}_files.xml";
   }
   async get({ identifier = null } = {}) {
     const url = this.API_PATTERN.replace(/{identifier}/g, identifier);
@@ -41,33 +57,62 @@ class FilesXmlAPI {
 
 class MetadataAPI {
   constructor() {
-    this.API_BASE = 'https://archive.org/metadata';
+    this.API_BASE = "https://archive.org/metadata";
   }
-  async get({ identifier = null, path = '' } = {}) {
+  async get({ identifier = null, path = "" } = {}) {
     return jsonFetch(`${this.API_BASE}/${identifier}/${path}`);
   }
 }
 
 class RelatedAPI {
   constructor() {
-    this.API_BASE = 'https://be-api.us.archive.org/mds/v1';
+    this.API_BASE = "https://be-api.us.archive.org/mds/v1";
   }
   async get({ identifier = null } = {}) {
-    return jsonFetch(`${this.API_BASE}/get_related/all/${identifier}`)
+    return jsonFetch(`${this.API_BASE}/get_related/all/${identifier}`);
   }
 }
 
 class SearchAPI {
   constructor() {
-    // this.API_BASE = 'TODO';
+    this.API_BASE = "https://archive.org/advancedsearch.php";
+    // ?q=test&fl%5B%5D=identifier&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=50&page=1&output=json&save=yes';
+  }
+  async get({
+    q = REQUIRED,
+    page = 1,
+    fields = ["identifier"],
+    ...options
+  } = {}) {
+    const reqOptions = {
+      q,
+      page,
+      fl: fields,
+      ...options,
+      output: "json",
+    };
+    let required = checkRequired(reqOptions);
+    if (required !== null) {
+      return { required };
+    }
+    const encodedParams = new URLSearchParams(reqOptions).toString();
+    const url = `${this.API_BASE}?${encodedParams}`;
+    log(url);
+    return jsonFetch(url);
   }
 }
 
+class SearchTextAPI {}
+class WaybackAPI {}
+
 module.exports = {
   AuthAPI: new AuthAPI(),
+  BookReaderAPI: new BookReaderAPI(),
   DetailsPageAPI: new DetailsPageAPI(),
   FilesXmlAPI: new FilesXmlAPI(),
   MetadataAPI: new MetadataAPI(),
   RelatedAPI: new RelatedAPI(),
-  SearchAPI: new SearchAPI()
+  SearchAPI: new SearchAPI(),
+  SearchTextAPI: new SearchTextAPI(),
+  WaybackAPI: new WaybackAPI(),
 };
--- test.js
+++ test.js
@@ -4,15 +4,27 @@ const {
   FilesXmlAPI,
   MetadataAPI,
   RelatedAPI,
-  SearchAPI
-} = require('./index');
+  SearchAPI,
+} = require("./index");
 
 const log = console.log;
-const identifier = 'example-png';
+const identifier = "example-png";
 
 (async () => {
-  log(await MetadataAPI.get({ identifier, path: 'metadata' }));
-  log(await MetadataAPI.get({ identifier, path: 'metadata/identifier' }));
+  log(await MetadataAPI.get({ identifier, path: "metadata" }));
+  log(await MetadataAPI.get({ identifier, path: "metadata/identifier" }));
   log(await RelatedAPI.get({ identifier }));
   log(await FilesXmlAPI.get({ identifier }));
+  log(
+    JSON.stringify(
+      await SearchAPI.get({
+        q: identifier,
+        fields: ["identifier"],
+        page: 2,
+        foo: "bar",
+      }),
+      null,
+      2
+    )
+  );
 })();
